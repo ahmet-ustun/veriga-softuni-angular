f102d44131b6b53cc0ed1d1eac7a607b28e5618d
ahmet-ustun <ahmetustunt@gmail.com>
2022-04-08 23:37:22 +0300

    styling the navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { AsideComponent } from './components/aside/aside.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AsideComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 
 export class AppModule { }
