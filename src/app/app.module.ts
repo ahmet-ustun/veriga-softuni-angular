240acce916f8336adbe852414d87aeb1b97be052
ahmet-ustun <ahmetustunt@gmail.com>
2022-04-09 09:34:50 +0300

    addinga weather component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AsideComponent } from './components/aside/aside.component';
 import { ClockComponent } from './components/clock/clock.component';
+import { WeatherComponent } from './components/weather/weather.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     AsideComponent,
-    ClockComponent
+    ClockComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule
