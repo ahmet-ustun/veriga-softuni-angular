4486843acb3ea6b6c9a9aaef2b1d02bf057954d8
ahmet-ustun <ahmetustunt@gmail.com>
2022-04-13 11:18:29 +0300

    initiating the dark theme logic

--- src/app/components/aside/aside.component.ts
+++ src/app/components/aside/aside.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { DarkModeService } from 'angular-dark-mode';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-aside',
-  templateUrl: './aside.component.html',
-  styleUrls: ['./aside.component.css']
+	selector: 'app-aside',
+	templateUrl: './aside.component.html',
+	styleUrls: ['./aside.component.css']
 })
 export class AsideComponent implements OnInit {
 
-  constructor() { }
+	darkMode$: Observable<boolean> = this.darkModeService.darkMode$;
 
-  ngOnInit(): void {
-  }
+	onToggle(): void {
+		this.darkModeService.toggle();
+	}
 
+	constructor(
+		private darkModeService: DarkModeService
+	) { }
+
+	ngOnInit(): void {
+
+	}
 }
